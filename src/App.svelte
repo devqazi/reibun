aa6758dfce8f4942604e3061b60244c8e6769e2f
Sayam Qazi <devqazi@gmail.com>
2023-05-22 13:12:14 +0500

    completed app

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,71 @@
 <script>
   import core10k from "./core10k";
+  let knownKanji = "一 九 七 二 人 入 八 力 十 下";
+  let vocab = [];
 
-  let knownKanji = "";
+  const handleGenerate = () => {
+    vocab = [];
+    let knownKanjiMap = {};
+    knownKanji.split("").forEach((kanji) => {
+      if (/^[\u4e00-\u9faf\u3400-\u4dbf]$/.test(kanji)) {
+        knownKanjiMap[kanji] = true;
+      }
+    });
+    for (let i in core10k) {
+      let word = core10k[i];
+      let kanjiInWord = word
+        .replace(/[^\u4e00-\u9faf\u3400-\u4dbf]+/, "")
+        .split("");
+      let containsKnownKanji = true;
+      for (let j in kanjiInWord) {
+        if (!knownKanjiMap[kanjiInWord[j]]) {
+          containsKnownKanji = false;
+          break;
+        }
+      }
+      if (containsKnownKanji) {
+        vocab.push(word);
+      }
+    }
+  };
+  handleGenerate();
 </script>
 
-<div>
+<main>
   <h3>Known Kanji</h3>
   <textarea bind:value={knownKanji} />
+  <button on:click={handleGenerate}>Generate</button>
   <h3>Generated Vocabulary</h3>
-</div>
+  <div>
+    {#each vocab as word}
+      <span>{word}</span>
+    {/each}
+  </div>
+</main>
+
+<style>
+  main {
+    padding: 16px;
+  }
+  textarea {
+    background-color: #303030;
+    color: white;
+    border: 0;
+    width: 100%;
+    padding: 5px;
+    margin-bottom: 5px;
+  }
+  textarea:focus {
+    outline: 0;
+  }
+  span {
+    margin: 0 8px 8px 0;
+    display: inline-block;
+    border: 1px solid #404040;
+    border-radius: 4px;
+    padding: 2px 5px;
+  }
+  button {
+    padding: 5px 10px;
+  }
+</style>
